4a6228a5cb5fcae6562bea019412542bdce8ea39
KwasniakJakub <jakub.kwasniak@wp.pl>
2022-06-06 19:54:08 +0200

    Laboratorium 6

---
+++ Laboratorium_6/Zadanie1.1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] argv) {
+        ArrayList<String> listaSlow = new ArrayList<String>();
+        Scanner scan1 = new Scanner(System.in);
+        while (scan1.hasNext()) {
+            for (String slowo : scan1.nextLine().split(" ")
+            ) {
+                listaSlow.add(slowo);
+            }
+        }
+        System.out.println(listaSlow.toString());
+    }
+}
--- Laboratorium_6/Zadanie2.2.java
+++ Laboratorium_6/Zadanie2.2.java
@@ -1,6 +1,5 @@
 import java.util.*;
 
-
 class Hamster {
     String name;
     private int hamsterNumber;
