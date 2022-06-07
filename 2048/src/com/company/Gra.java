3a33c6c4204ffce49bd94ef99c75aa7b6c914076
KwasniakJakub <jakub.kwasniak@wp.pl>
2022-06-07 12:53:44 +0200

    1 Etap tworzenia gry, napisane metody uruchamiające ramkę w AWT

---
+++ 2048/src/com/company/Gra.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.awt.*;
+
+public class Gra {
+    public Gra(){
+        init();
+    }
+    public void init(){
+
+    }
+    public void aktualizuj(){
+
+    }
+    public void renderuj(){
+
+    }
+    public void renderujText(Graphics2D gra){
+
+    }
+}
